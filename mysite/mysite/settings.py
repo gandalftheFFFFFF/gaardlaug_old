66bbe1d75e6628b1c093cdc7c5b06ddcba8bf745
Niels Pedersen <nielsschroderpedersen@gmail.com>
2015-07-20 20:08:01 +0200

    another static edit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -133,11 +133,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'fake_static')
 
-#STATICFILES_DIRS = (
-#	STATIC_PATH,
-#	)
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'static')
+	)
 
 
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'documents')
