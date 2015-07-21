b5aff714a9dd9583c6dce733f3559427ae75eac4
Niels Pedersen <nielsschroderpedersen@gmail.com>
2015-07-21 14:48:34 +0200

    made media work locally

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -142,14 +142,12 @@ STATICFILES_FINDERS = (
 
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'documents'),
 	)
 
 
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'documents')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'documents')
 
-#MEDIA_URL = '/documents/'
-
+MEDIA_URL = '/documents/'
 
 # IMPORT local_settings
 try:
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
 
     #url(r'^documents/', TemplateView.as_view(template_name='stat.html'))
     
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
